cdfd4b64e5cdd2106ef268c45d433b0bc9076e16
Daffa Ardiyansyah <111844855+DaffaArdiyansyah@users.noreply.github.com>
2026-01-13 11:13:03 +0700

    Ver.1
    Ini hanya tahap pertama pembelajaran dimana masih menyiapkan JS dan HTML

---
+++ script.js
@@ -0,0 +1,8 @@
+const products = [
+  { id: 1, name: "Laptop", price: 8000000, category: "Elektronik", stock: 5 },
+  { id: 2, name: "Mouse", price: 150000, category: "Elektronik", stock: 20 },
+  { id: 3, name: "Keyboard", price: 350000, category: "Elektronik", stock: 0 },
+  { id: 4, name: "Meja", price: 1200000, category: "Furniture", stock: 3 },
+  { id: 5, name: "Kursi", price: 700000, category: "Furniture", stock: 0 },
+];
+
